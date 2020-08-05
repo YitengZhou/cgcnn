39bac5cc34dfb41275a4ca7edfac7845155dfebf
zhouyiteng <542490754@qq.com>
2020-08-05 22:45:00 +0800

    finish elasticity

--- split.py
+++ split.py
@@ -2,13 +2,14 @@ import csv
 import math
 
 # function for splitting each property
-# new_file = open('test/elasticity/id_prop_total.csv', 'w', encoding='utf-8')
-# csv_writer = csv.writer(new_file)
+new_file = open('training/elasticity/elasticity_poisson.csv', 'w', encoding='utf-8')
+csv_writer = csv.writer(new_file)
 
-# with open('test/elasticity/id_prop_total.csv', 'r', encoding='utf-8') as f:
-#     reader = csv.reader(f)
-#     for row in reader:
-#         line = []
-#         line.append(row[0]) # id
-#         line.append(row[5]) # property
-#         csv_writer.writerow(line)
+with open('training/elasticity/elasticity.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        line = []
+        line.append(row[0]) # id
+        line.append(row[13]) # property
+        # line.append(math.log(float(row[11])))  # log - G, K
+        csv_writer.writerow(line)
